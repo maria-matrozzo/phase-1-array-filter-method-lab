0cd018aa2dc6b3933c59f73db0053a72e8fda294
Maria <mmatrozzo@gmail.com>
2021-11-04 12:52:15 -0400

    commit tree ed2fbabd456611b7ffac115cbb481c2e3aee2d08

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    return drivers.filter(function(driver) {
+       return driver.toLowerCase() === string.toLowerCase()
+    })
+ }
+
+ function fuzzyMatch(driver, string) {
+    let correctLetters = driver.filter(beginningLetters => beginningLetters.substring(0,2) === string)
+return correctLetters
+}
+ 
+//  function matchName(drivers, string) {
+//      return drivers.filter(function(driver){
+//              return drivers.name && drivers.hometown;
+//      }
+//     }
+
+//  matchName(drivers, 'Bobby');
